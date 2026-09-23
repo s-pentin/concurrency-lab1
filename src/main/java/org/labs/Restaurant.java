6e62adad1fbdaa21fd6c6cfc3fe7511f6848878d
s-pentin <s5.pentin@yandex.ru>
2026-09-24 00:38:33 +0300

    used virtual thread for waiters

--- src/main/java/org/labs/Restaurant.java
+++ src/main/java/org/labs/Restaurant.java
@@ -2,58 +2,49 @@ package org.labs;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Restaurant {
 
-    private final AtomicBoolean lunchFinished = new AtomicBoolean(false);
     private final ExecutorService waiters;
+    private final Semaphore semaphore;
     private final AtomicInteger soupCount;
     private final int waiterDelayMillis;
 
     public Restaurant(int countWaiters, int soupCount, int waiterDelayMillis) {
-        this.waiters = Executors.newFixedThreadPool(countWaiters);
+        this.waiters = Executors.newVirtualThreadPerTaskExecutor();
+        this.semaphore = new Semaphore(countWaiters, true);
         this.soupCount = new AtomicInteger(soupCount);
         this.waiterDelayMillis = waiterDelayMillis;
-    }
-
-    public boolean takeSoup() {
-        while (!lunchFinished.get()) {
-            int current = soupCount.get();
-
-            if (current <= 0) {
-                lunchFinished.set(true);
-                return false;
-            }
-
-            if (soupCount.compareAndSet(current, current - 1)) {
-                if (current == 1) {
-                    lunchFinished.set(true);
-                }
-                return true;
-            }
-        }
-        return false;
     }
 
     public SoupRequest requestSoup(int programmerId) {
         SoupRequest request = new SoupRequest(programmerId);
 
         waiters.submit(() -> {
+            boolean acquired = false;
+
             try {
+                semaphore.acquire();
+                acquired = true;
+
                 Thread.sleep(waiterDelayMillis);
 
                 if (takeSoup()) {
                     request.complete(SoupResult.SOUP_RECEIVED);
                 } else {
-                    System.out.println("Заказы больше не принимаются / суп закончился");
                     request.complete(SoupResult.LUNCH_FINISHED);
                 }
 
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 request.getResult().completeExceptionally(e);
+
+            } finally {
+                if (acquired) {
+                    semaphore.release();
+                }
             }
         });
 
@@ -62,5 +53,10 @@ public class Restaurant {
 
     public void shutdown() {
         waiters.shutdown();
+    }
+
+    boolean takeSoup() {
+        int previous = soupCount.getAndUpdate(count -> count > 0 ? count - 1 : 0);
+        return previous > 0;
     }
 }
