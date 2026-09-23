e8cc2def22c52b74e4bf52cc6e326fd52d28df2a
s-pentin <s5.pentin@yandex.ru>
2026-09-24 00:37:45 +0300

    fixed SpoonManager, use a lock for each spoon

--- src/main/java/org/labs/SpoonManager.java
+++ src/main/java/org/labs/SpoonManager.java
@@ -1,46 +1,28 @@
 package org.labs;
 
-import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SpoonManager {
-    private final ReentrantLock reentrantLock = new ReentrantLock(true);
-    private final Condition spoonsAvailable = reentrantLock.newCondition();
-    private final boolean[] usedSpoons;
+    private final ReentrantLock[] reentrantLocksSpoon;
 
     public SpoonManager(int spoonCount) {
-        this.usedSpoons = new boolean[spoonCount];
+        reentrantLocksSpoon = new ReentrantLock[spoonCount];
+        for (int i = 0; i < spoonCount; i++) {
+            this.reentrantLocksSpoon[i] = new ReentrantLock(true);
+        }
+    }
+
+    public void acquire(int leftSpoon, int rightSpoon) {
+        int first = Math.min(leftSpoon, rightSpoon);
+        int second = Math.max(leftSpoon, rightSpoon);
+        reentrantLocksSpoon[first].lock();
+        reentrantLocksSpoon[second].lock();
     }
 
     public void release(int leftSpoon, int rightSpoon) {
-        reentrantLock.lock();
-        try {
-            usedSpoons[leftSpoon] = false;
-            usedSpoons[rightSpoon] = false;
-            spoonsAvailable.signalAll();
-        } finally {
-            reentrantLock.unlock();
-        }
-    }
-
-    public void acquire(int leftSpoon, int rightSpoon) throws InterruptedException {
-        reentrantLock.lock();
-        try {
-            while (!tryAcquireWithoutLock(leftSpoon, rightSpoon)) {
-                spoonsAvailable.await();
-            }
-        } finally {
-            reentrantLock.unlock();
-        }
-    }
-
-    private boolean tryAcquireWithoutLock(int leftSpoon, int rightSpoon) {
-        if (!usedSpoons[leftSpoon] && !usedSpoons[rightSpoon]) {
-            usedSpoons[leftSpoon] = true;
-            usedSpoons[rightSpoon] = true;
-            return true;
-        } else {
-            return false;
-        }
+        int first = Math.min(leftSpoon, rightSpoon);
+        int second = Math.max(leftSpoon, rightSpoon);
+        reentrantLocksSpoon[second].unlock();
+        reentrantLocksSpoon[first].unlock();
     }
 }
