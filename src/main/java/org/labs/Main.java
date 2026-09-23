cf60f49250dd769092a88aab927a53c69f0f1a44
s-pentin <s5.pentin@yandex.ru>
2026-09-24 00:40:46 +0300

    used virtual thread and mealManager

--- src/main/java/org/labs/Main.java
+++ src/main/java/org/labs/Main.java
@@ -14,29 +14,28 @@ public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant(WAITER_COUNT, SOUP_COUNT, WAITER_DELAY_MILLIS);
         SpoonManager spoonManager = new SpoonManager(SPOON_COUNT);
-        ExecutorService programmerExecutor = Executors.newFixedThreadPool(PROGRAMMER_COUNT);
-
-        for (int i = 0; i < PROGRAMMER_COUNT; i++) {
-            Programmer programmer = new Programmer(i, restaurant, spoonManager, SPOON_COUNT);
-            programmerExecutor.submit(programmer);
-        }
-
-        programmerExecutor.shutdown();
-
-        try {
-            boolean terminated = programmerExecutor.awaitTermination(1, TimeUnit.MINUTES);
-            if (!terminated) {
-                programmerExecutor.shutdownNow();
-
-                if (!programmerExecutor.awaitTermination(15, TimeUnit.SECONDS)) {
-                    System.out.println("Не удалось корректно завершить programmerExecutor");
-                }
+        MealManager mealManager = new MealManager(PROGRAMMER_COUNT);
+        try (ExecutorService programmerExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < PROGRAMMER_COUNT; i++) {
+                Programmer programmer = new Programmer(i, restaurant, spoonManager, mealManager, SPOON_COUNT);
+                programmerExecutor.submit(programmer);
             }
-        } catch (InterruptedException e) {
-            programmerExecutor.shutdownNow();
-            Thread.currentThread().interrupt();
-        } finally {
-            restaurant.shutdown();
+
+            try {
+                boolean terminated = programmerExecutor.awaitTermination(1, TimeUnit.MINUTES);
+                if (!terminated) {
+                    programmerExecutor.shutdownNow();
+
+                    if (!programmerExecutor.awaitTermination(15, TimeUnit.SECONDS)) {
+                        System.out.println("Не удалось корректно завершить programmerExecutor");
+                    }
+                }
+            } catch (InterruptedException e) {
+                programmerExecutor.shutdownNow();
+                Thread.currentThread().interrupt();
+            } finally {
+                restaurant.shutdown();
+            }
         }
     }
 }
